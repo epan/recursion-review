7fe370fd54e2aa874321b1e587de2bbd837041c5
MKS-Student <mks.student@hackreactor.com>
2017-03-20 21:03:50 -0700

    parseJSON started

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,6 +2,34 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
+
+var testdata = '["one"]';
+// => ['one'];
+var testdata2 = '{"foo": "bar"}';
+
 var parseJSON = function(json) {
-  // your code goes here
+  var firstChar = json[0];
+
+  if (firstChar === '[') {
+    // console.log('I think it\'s an array')
+    var result = [];
+    result.push(json[i+1]);
+
+    // return result;
+    return new Array(result.join(''));
+
+  }
+
+  if (firstChar === '"') {
+    var substring = json.substring(i + 1);
+    var cutAt = substring.indexOf('"');
+    substring = substring.slice(0, cutAt);
+    return substring;
+  }
+
+  if (firstChar === '{') {
+    var objResult = {};
+    return objResult;
+  }
+
 };
